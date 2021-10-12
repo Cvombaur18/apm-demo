c09b63e77dffe49c6544e86e5d187b8e3677d765
Chris Vombaur <cvombaur@gmail.com>
2021-10-12 10:14:50 -0600

    rollbar

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+const Rollbar = require('rollbar');
+const rollbar = new Rollbar( {
+    accessToken: 'ff619d95f0dc465fad521e1981fe9dac',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
 })
 
-const port = process.env.port || 4400
+const port = process.env.PORT || 4400
 
 app.listen(port, () => 
 console.log(`server is up and runnning on ${port}!`));

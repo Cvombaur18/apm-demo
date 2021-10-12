933605809c77cd2a3deeef9a705375325ddc2845
Chris Vombaur <cvombaur@gmail.com>
2021-10-12 10:28:15 -0600

    added more

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const Rollbar = require('rollbar');
+
 const rollbar = new Rollbar( {
     accessToken: 'ff619d95f0dc465fad521e1981fe9dac',
     captureUncaught: true,
@@ -12,6 +13,19 @@ const app = express();
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
+})
+
+const students = []
+
+app.post('api/student', (req, res) => {
+    let { name } = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('student added successfully')
+
+    res.status(200).send(students)
 })
 
 const port = process.env.PORT || 4400

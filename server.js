a2ba5b9cdaac89046854d38ec0e1cc228f2d6ce6
Chris Vombaur <cvombaur@gmail.com>
2021-10-12 15:52:08 -0600

    style

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json())
 
-app.use('/'), express.static((path.join(__dirname, '/public/styles.css')))
+app.use('/style'), express.static((path.join(__dirname, './public/styles.css')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))

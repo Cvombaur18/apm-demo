34a853ed5c1b0945026d8642cdbcedd5bbb3b20b
Chris Vombaur <cvombaur@gmail.com>
2021-10-12 10:39:54 -0600

    added json

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const rollbar = new Rollbar( {
 
 const app = express();
 
-app.use(express,json())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
